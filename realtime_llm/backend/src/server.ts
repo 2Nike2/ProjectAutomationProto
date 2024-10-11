5f0764506f34e7be41d1e9d2bbf3abf182283573
2Nike2 <29421346+2Nike2@users.noreply.github.com>
2024-10-11 23:32:40 +0900

    各種サーバイベントの内容確認、および入力音声書き起こし確認

--- realtime_llm/backend/src/server.ts
+++ realtime_llm/backend/src/server.ts
@@ -37,7 +37,10 @@ wss.on('connection', (ws) => {
     openai_ws.send(JSON.stringify({
       type: "session.update",
       "session": {
-        "instructions": " 日本語でやりとりしてください。"
+        "instructions": " 日本語でやりとりしてください。",
+        "input_audio_transcription": { // この設定で入力音声の文字起こしの確認もしたいができない?また公式で記述のあった、enabled属性がない
+          "model": "whisper-1"
+        },
       }
     }));
   });
@@ -82,6 +85,77 @@ wss.on('connection', (ws) => {
     if(event['type'] === 'error'){
       console.log(event)
     }
+
+    // 各種 Server event 確認
+    // セッション関連
+    // if(event["type"] === 'session.created'){
+    //   console.log(event)
+    // }
+    // if(event["type"] === 'session.updated'){
+    //   console.log(event)
+    // }
+
+    // 入力音声認識関連
+    // VAD(Voice Activity Detection)の場合
+    // 1. input_audio_buffer.speech_started : 音声開始検出
+    // 2. input_audio_buffer.speech_stopped : 音声終了検出
+    // 3. input_audio_buffer.speech_comited : 音声部分確定
+    // if(event["type"] === 'input_audio_buffer.speech_started'){
+    //   console.log(event)
+    // }
+    // if(event["type"] === 'input_audio_buffer.speech_stopped'){
+    //   console.log(event)
+    // }
+    // if(event["type"] === 'input_audio_buffer.committed'){
+    //   console.log(event)
+    // }
+
+    // 会話データ関連
+    // if(event["type"] === 'conversation.item.created'){
+    //   // console.log(event)
+    //   console.log(event['item']['content']) // ここで入力音声の文字起こしの結果を見たいけど上手くいかない?
+    // }
+
+    // // 応答関連
+    // if(event["type"] === 'response.created'){ // 最初の"in_progress"の応答が作成されたときに返ってくる、つまり応答の開始?
+    //   console.log(event)
+    // }
+
+    // // 応答アイテム関連
+    // if(event["type"] === 'response.output_item.added'){
+    //   console.log(event)
+    // }
+    // if(event["type"] === 'response.output_item.done'){
+    //   // console.log(event)
+    //   console.log(event['item']['content'][0]['transcript']) // ここで出力文字列を取ることも可能
+    // }
+
+    // // 応答コンテンツ部分関連
+    // if(event["type"] === 'response.content_part.added'){
+    //   console.log(event)
+    // }
+    // if(event["type"] === 'response.content_part.done'){
+    //   // console.log(event)
+    //   console.log(event['part']['transcript'])
+    // }
+
+    // 応答文字起こし関連
+    // if(event["type"] === 'response.audio_transcript.delta'){ // 数文字程度で部分的に逐次出力される
+    //   console.log(event)
+    // }
+    // if(event["type"] === 'response.audio_transcript.done'){
+    //   // console.log(event)
+    //   console.log(event['transcript']) // ここで出力文字列を取ることも可能
+    // }
+    
+    // 応答音声関連
+    // if(event["type"] === 'response.audio.delta'){ // 音声断片が部分的に逐次出力される
+    //   // console.log(event) // deltaの表示が煩雑になるのでコメントアウト推奨
+    // }
+    // if(event["type"] === 'response.audio.done'){
+    //   console.log(event)
+    // }
+
     if(event["type"] === 'response.done' && event["response"]["status"] == 'completed'){
       console.log(event['response']['output'][0]['content'][0]['transcript'])
     }
